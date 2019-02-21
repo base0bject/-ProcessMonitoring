3d6638400d7b9639cb56332c2dcedd9deebdd84b
hohomonix23274 <nikitamanukol23274@gmail.ru>
2019-02-21 18:18:11 +0300

    last commit

---
+++ Testing/Testing/Testing.cpp
@@ -0,0 +1,26 @@
+﻿// Testing.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "windows.h"
+#include "iostream"
+#include "windows.h"
+#include <windows.h>
+
+#define MEMSIZE 1
+
+using namespace std;
+
+void main()
+{
+	void *p = NULL;
+	DWORD old;
+	p = VirtualAlloc(NULL, MEMSIZE, MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+	Sleep(10);
+
+	p = VirtualAlloc(NULL, MEMSIZE, MEM_COMMIT, PAGE_READWRITE);
+	Sleep(10);
+
+	VirtualProtect(p, MEMSIZE, PAGE_EXECUTE_READ, &old);
+	Sleep(10);
+	system("pause");
+}
